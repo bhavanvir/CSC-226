877a27f9332a4191d3993e3606f9761a88fcdf67
Bhavanvir Rai <bhavanvir.r@gmail.com>
2022-03-17 22:05:36 -0700

    Add files via upload

--- Assignment 3 - Programming/PrimsAlg.java
+++ Assignment 3 - Programming/PrimsAlg.java
@@ -1,5 +1,4 @@
 import edu.princeton.cs.algs4.*;
-
 import java.io.*;
 
 // We implement Prim's algorithm using data structures implemented in edu.princeton.cs.algs4.
@@ -15,9 +14,12 @@ public class PrimsAlg {
         In in = new In(args[0]);
         EdgeWeightedGraph G = new EdgeWeightedGraph(in);
         boolean[] marked = new boolean[G.V()]; // In the early Monday tutorial, Boolean is a class whereas boolean is a
-                                               // primitive.
         Queue<Edge> mst = new Queue<Edge>(); // A linked list could also work.
         marked[0] = true;
+
+        boolean[] markedM = new boolean[G.V()];
+        Queue<Edge> mstM = new Queue<Edge>();
+        markedM[0] = true;
 
         while (true) {
             // In order to find a minimum crossing edge, we loop through all the edges in
@@ -29,10 +31,8 @@ public class PrimsAlg {
                 int u = e.either();
                 int v = e.other(u);
                 // B) Check the condition if they are cross edges and insert to pq
-                assert marked[v] || marked[v];
-                if (marked[u] && marked[v])
-                    continue;
-                minpq.insert(e);
+                if ((!marked[u] && marked[v]) || (marked[u] && !marked[v]))
+                    minpq.insert(e);
             }
 
             // If the priority queue is empty, then this means there are no more crossing
@@ -45,37 +45,95 @@ public class PrimsAlg {
             // minimum crossing edge.
 
             // C) get the edge minimum from pq
-            Edge eMin = minpq.min();
+            Edge eMin = minpq.delMin();
 
             // D) Update the (current) MST and Mark the other vertex
-
-            int v = eMin.either(), u = eMin.other(v);
-
+            int u = eMin.either();
+            int v = eMin.other(u);
             if (!marked[u]) {
-                assert !marked[u];
                 marked[u] = true;
+                mst.enqueue(eMin);
                 for (Edge e : G.adj(u))
                     if (!marked[e.other(u)])
-                        mst.enqueue(e);
+                        minpq.insert(e);
             }
             if (!marked[v]) {
-                assert !marked[v];
                 marked[v] = true;
+                mst.enqueue(eMin);
                 for (Edge e : G.adj(v))
                     if (!marked[e.other(v)])
-                        mst.enqueue(e);
+                        minpq.insert(e);
             }
+
+        }
+
+        while (true) {
+            // In order to find a crossing edge, we loop through all the edges in
+            // the graph and
+            // add all of the crossing edges to a priority queue.
+
+            MaxPQ<Edge> maxpq = new MaxPQ<Edge>();
+
+            for (Edge e : G.edges()) {
+                int u = e.either();
+                int v = e.other(u);
+                // B) Check the condition if they are cross edges and insert to pq
+                if ((!markedM[u] && markedM[v]) || (markedM[u] && !markedM[v]))
+                    maxpq.insert(e);
+            }
+
+            // If the priority queue is empty, then this means there are no more crossing
+            // edges so we are done.
+            if (maxpq.isEmpty()) {
+                break;
+            }
+
+            // If the priority queue is not empty, then the element in it is a
+            // crossing edge.
+
+            // C) get the edge from pq
+            Edge eMax = maxpq.delMax();
+
+            // D) Update the (current) MST and Mark the other vertex
+            int u = eMax.either();
+            int v = eMax.other(u);
+            if (!markedM[u]) {
+                markedM[u] = true;
+                mstM.enqueue(eMax);
+                for (Edge e : G.adj(u))
+                    if (!markedM[e.other(u)])
+                        maxpq.insert(e);
+            }
+            if (!markedM[v]) {
+                markedM[v] = true;
+                mstM.enqueue(eMax);
+                for (Edge e : G.adj(v))
+                    if (!markedM[e.other(v)])
+                        maxpq.insert(e);
+            }
+
         }
 
         // Once an MST has been found, print its edges and total weight.
-        LazyPrimMST mstL = new LazyPrimMST(G);
-        double edgeWeight = 0.0;
-        for (Edge e : mstL.edges()) {
+        double eWeight = 0.0;
+        for (Edge e : mst) {
             StdOut.println(e);
-            edgeWeight += e.weight();
+            eWeight += e.weight();
         }
         // E) Evaluate the weight of MST
-        StdOut.printf("Total weight is=%.5f\n", edgeWeight);
+        StdOut.printf("Total weight is=%.5f\n", eWeight);
+
+        System.out.println();
+
+        // Once an MST has been found, print its edges and total weight.
+        eWeight = 0.0;
+        for (Edge e : mstM) {
+            StdOut.println(e);
+            eWeight += e.weight();
+        }
+        // E) Evaluate the weight of MST
+        StdOut.printf("Total weight is=%.5f\n", eWeight);
+
     }
 
 }
